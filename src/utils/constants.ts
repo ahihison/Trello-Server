1c420f90cc5a5f3dfa811b47b006e2e38ca5cc9a
Sơn <88760761+ahihison@users.noreply.github.com>
2024-03-03 21:47:13 +0700

    Add new domain to WHITELIST_DOMAINS

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,8 @@
 
 //domain can be accessed by the server
 export const WHITELIST_DOMAINS = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://trello-web-chi.vercel.app/'
   
 ];
 
