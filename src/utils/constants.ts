a12b35cee249631722c0703238f9c52fa3cbeb21
Sơn <88760761+ahihison@users.noreply.github.com>
2024-03-03 21:54:42 +0700

    Update WHITELIST_DOMAINS in constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,7 @@
 
 //domain can be accessed by the server
 export const WHITELIST_DOMAINS = [
-    'http://localhost:3000',
-    'https://trello-web-chi.vercel.app/'
-  
+    'https://trello-web-chi.vercel.app'
 ];
 
 export const BOARD_TYPES = {
